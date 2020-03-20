b215b1908c87875222a28e9bee96619a094c286d
LeCoding-tech <59509041+LeCoding-tech@users.noreply.github.com>
2020-03-20 11:49:27 -0400

    Made changes to hard challenge

--- JavaScript-Challenges (Week 6)/Hard-Challenge/script.js
+++ JavaScript-Challenges (Week 6)/Hard-Challenge/script.js
@@ -2,22 +2,22 @@ function add(a, b){
     return (a + b);
   }
   
-  console.log(add(1,20))
+  console.log(add(1,20));
   
   function subtract(a, b){
     return (a - b)
   }
   
-  console.log(subtract(9,18))
+  console.log(subtract(9,18));
   
   function multiply(a, b){
     return (a * b)
   }
   
-  console.log(multiply(4,9))
+  console.log(multiply(4,9));
   
   function divide(a, b){
     return(a / b)
   }
   
-  console.log(subtract(9,3))
+  console.log(divide(9,3));
