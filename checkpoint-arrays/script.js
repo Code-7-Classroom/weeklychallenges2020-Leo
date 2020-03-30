92e1d61ea3eab08b75812c2ab2519ee760f3377c
LeCoding-tech <59509041+LeCoding-tech@users.noreply.github.com>
2020-03-30 11:11:01 -0400

    arrays chalenge

---
+++ checkpoint-arrays/script.js
@@ -0,0 +1,14 @@
+
+var drinks = [];
+
+drinks.push("Coffee ", "Water ", "Tea ");
+
+var foods = [];
+
+foods.push("Apples ", "Sandwich ", "Chips ");
+
+var meals =  drinks + foods;
+
+meals.reverse;
+
+console.log(meals);
